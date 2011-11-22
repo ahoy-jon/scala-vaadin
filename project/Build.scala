cd9fe1e904b3b45f7ee36bf7d2159c4fdd84025d
Jonathan Winandy <jonathan.winandy@gmail.com>
2011-11-22 07:28:59 +0100

    update webplugin

--- project/Build.scala
+++ project/Build.scala
@@ -1,6 +1,13 @@
 import sbt._
+import com.github.siasia._
+import WebPlugin._
 import Keys._
+
+
+
+
 object ScalaVaadinBuild extends Build {
+  resolvers += ScalaToolsReleases
 
   override def projects = Seq(root, core, demo)
 
@@ -23,23 +30,23 @@ object ScalaVaadinBuild extends Build {
     id = "demo",
     base = file("demo"),
     dependencies = Seq(core),
-    settings = standardSettings ++ Seq(libraryDependencies ++= Seq(Dependency.jetty)) ++ WebPlugin.webSettings
+    settings = standardSettings ++ Seq(libraryDependencies ++= Seq(Dependency.jetty)) ++ webSettings
   )
 
 
   object Dependency {
     val vaadin = "com.vaadin" % "vaadin" % "6.6.0"
     val scalatest = "org.scalatest" % "scalatest_2.9.0" % "1.6.1" % "test"
-    val jetty = "org.mortbay.jetty" % "jetty" % "6.1.22" % "jetty"
-    val log  = "com.weiglewilczek.slf4s" %% "slf4s" % "1.0.6"
+    val jetty = "org.mortbay.jetty" % "jetty" % "6.1.22" % "container"
+    val log  = "com.weiglewilczek.slf4s" %% "slf4s" % "1.0.7"
     val slf4j = "org.slf4j" % "slf4j-log4j12" % "1.6.1"% "test"
   }
 
 
   lazy val standardSettings = Defaults.defaultSettings ++ Seq(
     organization := "org.scalavaadin",
-    version := "0.0.2",
-    scalaVersion := "2.9.0-1",
+    version := "0.0.3",
+    scalaVersion := "2.9.1",
     scalacOptions ++= Seq("-encoding", "UTF-8", "-deprecation", "-unchecked")
 
   )
--- project/plugins/build.sbt
+++
@@ -1,7 +0,0 @@
-resolvers += "Web plugin repo" at "http://siasia.github.com/maven2"
-
-libraryDependencies <+= sbtVersion("com.github.siasia" %% "xsbt-web-plugin" % _)
-
-resolvers += "sbt-idea-repo" at "http://mpeltonen.github.com/maven/"
-
-libraryDependencies += "com.github.mpeltonen" %% "sbt-idea" % "0.10.0-SNAPSHOT"
